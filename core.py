2eafc19d7a2cf4a7397f304f73b496f573cb0483
Henry Xie <henry@yipit.com>
2013-10-21 12:54:40 -0400

    headers

--- core.py
+++ core.py
@@ -1,10 +1,14 @@
 from xlrd import open_workbook
 
+NUM_COLUMNS = 16
+HEADER_ROW = 22
+
 
 class DataCleaner(object):
     def __init__(self, filename):
         self.filename = filename
         self.wb = open_workbook(filename)
+        self.sheet = self.wb.sheets()[0]
 
     def process(self):
         headers = self.get_headers()
@@ -15,4 +19,9 @@ class DataCleaner(object):
         """
         Returns a tuple of the column names
         """
-        pass
+        headers = []
+        for col in xrange(NUM_COLUMNS):
+            headers.append(
+                str(self.sheet.cell(HEADER_ROW, col).value))
+
+        return headers
