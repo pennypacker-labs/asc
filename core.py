cde32cb69c5e2168eff2c21c3685652d4acecd83
Suneel Chakravorty <suneel0101@gmail.com>
2013-10-21 12:53:23 -0400

    updating with more pseudocode

--- core.py
+++ core.py
@@ -12,8 +12,9 @@ class DataCleaner(object):
 
     def process(self):
         headers = self.get_headers()
-        rows = self.get_rows()
-        self.output_to_csv(headers, rows)
+        rows = self.get_rows_from_workbook()
+        row_data = self.get_data_from_rows(rows)
+        self.output_to_csv(headers, row_data)
 
     def get_headers(self):
         """
@@ -23,5 +24,24 @@ class DataCleaner(object):
         for col in xrange(NUM_COLUMNS):
             headers.append(
                 str(self.sheet.cell(HEADER_ROW, col).value))
-
         return headers
+
+    def get_data_from_rows(self, rows):
+        """
+        Gets each line item from each purchase order.
+        """
+        line_items = []
+        vendor = None
+        date = None
+        order_number = None
+        for row in rows:
+            if self.is_purchase_order_row():
+                vendor = self.extract_vendor(row)
+                date = self.extract_date(row)
+                order_number = self.extract_order_number(row)
+                continue
+            elif self.is_line_item_row():
+                fields = self.get_fields(row)
+                fields.extend([vendor, date, order_number])
+                line_items.append(fields)
+        return line_items
